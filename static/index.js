04876b03d63b01d56fd3b42f8154ebcfa9c43848
0x8D <lit17909@proton.me>
2026-05-06 22:18:41 -0300

    feat: now it also shows the quote category

--- static/index.js
+++ static/index.js
@@ -1,9 +1,11 @@
 const frase = document.getElementById("frase")
+const categoria = document.getElementById("categoria")
 
 const getTarefa = () => {
   fetch("/get-tarefa")
   .then(response => response.json())
   .then(data => {
     frase.innerHTML = data.data[0].quote
+    categoria.innerHTML = "Category: " + data.data[0].category
   })
 }
