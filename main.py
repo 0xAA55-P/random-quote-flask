c94469a987fce1309ed6b292de592a5418639ead
0x8D <lit17909@proton.me>
2026-05-16 23:18:59 -0300

    refactor: improved code formatting and error handling

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
+"""BackEnd Principal
+
+Retorna a frase aleatória gerada em um JSON
+
+Rotas:
+    /: Ponto inicial
+    /get-tarefa: Retorna a tarefa ao acessar
+
+Dev: Anna
+Github: https://github.com/0xAA55-P
+Data de atualização: 16/05/2026
+"""
+
 from flask import Flask, render_template, jsonify
 import requests
 import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/get-tarefa")
 def get_tarefa():
-  resposta = requests.get("https://quote-generator-api-six.vercel.app/api/quotes/?limit=1")
+    try:
+        resposta = requests.get(
+            "https://quote-generator-api-six.vercel.app/api/quotes/?limit=1", timeout=5
+        )
 
-  if resposta.status_code != 200:
-    return {}
+        if resposta.status_code != 200:
+            return {}
 
-  return jsonify(resposta.json())
+        return jsonify(resposta.json())
+
+    except requests.Timeout:
+        return {}
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
